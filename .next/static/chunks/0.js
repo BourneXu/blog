be3cdfc4479f9ffaee4d084d41aae7722fae6b1a
Chao <chaoxu18@vt.edu>
2020-01-26 12:42:27 -0500

    Init page

---
+++ .next/static/chunks/0.js
@@ -0,0 +1,16 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./.yarn/$$virtual/next-virtual-365cff0b65/0/cache/next-npm-9.2.1-9e2a04bc32-1.zip/node_modules/next/dist/client/dev/noop.js":
+/*!***********************************************************************************************************************************!*\
+  !*** ./.yarn/$$virtual/next-virtual-365cff0b65/0/cache/next-npm-9.2.1-9e2a04bc32-1.zip/node_modules/next/dist/client/dev/noop.js ***!
+  \***********************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=0.js.map
