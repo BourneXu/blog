90a5ab9a98ff62c6f054abb0edec4ed32b89bb54
Chao <chaoxu18@vt.edu>
2020-01-26 13:07:53 -0500

    Update: about page

--- templates/about.js
+++ templates/about.js
@@ -5,14 +5,16 @@ import theme from '../theme'
 
 export default meta => ({ children }) => <MDXProvider components={theme.components}><>
   <Head>
-    <title>{meta.title}</title>
+    <title>Chao's Blog | {meta.title}</title>
     <meta property="og:description" content={meta.description} />
-    <meta name="twitter:description" content={meta.description} />    
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content="Chao Xu" />
+    <meta name="twitter:description" content={meta.description} />
   </Head>
   <theme.Container>
-    <theme.components.h1>{meta.title}</theme.components.h1>
-    <theme.Nav url={meta.url}/>
+    <theme.components.h1>Chao Xu</theme.components.h1>
+    <theme.Nav url={meta.url} />
     <theme.Content>{children}</theme.Content>
-    <theme.Footer/>
+    <theme.Footer />
   </theme.Container>
 </></MDXProvider>
